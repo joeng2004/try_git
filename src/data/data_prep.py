2eebc3c8f6f0d01fb66866a06bd8a078e6308e83
joeng2004 <joeng2004@gmail.com>
2023-09-20 15:20:29 +0800

    try .ignore

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
-
+import os
 
 df = pd.DataFrame({"A":[1,2,3], "B":[1,2,3]})
 
@@ -11,3 +11,10 @@ df2 = pd.DataFrame({"C":[2,3,5], "B":[3,4,5]})
 df3 = pd.DataFrame({"C":[2,3,5, 8], "B":[3,4,5]})
 df2.head()
  
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
+
+api_key = os.getenv("apikey")
