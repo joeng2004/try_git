9e8f9a38014f23301ecb7ed74dcf09ee0702c889
joeng2004 <joeng2004@gmail.com>
2023-09-20 12:53:48 +0800

    my 1st add to dev

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -7,4 +7,7 @@ df = pd.DataFrame({"A":[1,2,3], "B":[1,2,3]})
 
 df2 = pd.DataFrame({"C":[2,3,5], "B":[3,4,5]})
 
+#New features added
 df3 = pd.DataFrame({"C":[2,3,5, 8], "B":[3,4,5]})
+df2.head()
+ 
